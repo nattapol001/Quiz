931c2115f80c3c82fb999ba59580c9b7192c65d4
Bailey <nattapolprom02@gmial.com>
2023-04-26 14:20:36 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class MainClass{
+    static void Main() {
+        Console.Write("Enter the number of stalls: ");
+        int n =
+        int.Parse(Console.ReadLine());
+        bool[] stalls = new bool[n];
+
+        while(true){
+            Console.Write("Enter the stall number (0 to exit): ");
+            int stallNum1 =
+            int.Parse(Console.ReadLine());
+            int stallNum2 = 0;
+
+            if(stallNum1 ==0){
+                Console.WriteLine("Exiting program...");
+                break;
+            }
+            if (stallNum2 == 0) {
+        Console.WriteLine("Reserving Stall " + stallNum1 + "...");
+        if (stalls[stallNum1 - 1]) {
+          Console.WriteLine("The stall is reserved.");
+        } else {
+          stalls[stallNum1 - 1] = true;
+          Console.WriteLine("The stall is reserved.");
+        }
+      } else if (stallNum2 < 0 || stallNum2 >= n) {
+        Console.WriteLine("Invalid input. Stall number must be between 1 and " + n + ".");
+        continue;
+      } else {
+        Console.WriteLine("Reserving Stalls " + stallNum1 + " and " + stallNum2 + "...");
+        if (stalls[stallNum1 - 1] || stalls[stallNum2 - 1]) {
+          Console.WriteLine("The stall is reserved.");
+        } else {
+          int start = Math.Min(stallNum1, stallNum2);
+          int end = Math.Max(stallNum1, stallNum2);
+          bool canReserve = true;
+          for (int i = start - 1; i < end; i++) {
+            if (stalls[i]) {
+              Console.WriteLine("The entrance can’t be reserved.");
+              canReserve = false;
+              break;
+            }
+          }
+          if (canReserve) {
+            for (int i = start - 1; i < end; i++) {
+              stalls[i] = true;
+            }
+            Console.WriteLine("The stalls are reserved.");
+          }
+        }
+      }
+
+      Console.Write("Stalls: ");
+      for (int i = 0; i < n; i++) {
+        if (stalls[i]) {
+          Console.Write("X ");
+        } else {
+          Console.Write(i + 1 + " ");
+        }
+      }
+      Console.WriteLine();
+    }
+  }
+}
